8df8d8825493cf9e660372800d0df9b055db8454
SebiSomu <sebigsomu@gmail.com>
2025-12-18 18:44:25 +0200

    Successfully set up DaisyUI configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import Button from "./Button";
+
 const App = () => {
     return (
         <div className="min-h-screen bg-[#e0e7ff] flex flex-col items-center justify-center">
             <h1 className="relative text-5xl md:text-7xl font-black text-slate-800 tracking-tighter inline-block
-                     after:content-[''] after:absolute after:left-0 after:bottom-1 after:w-full after:h-[4px] after:bg-slate-800">
+                     after:content-[''] after:absolute after:left-0 after:bottom-1 after:w-full after:h-1 after:bg-slate-800">
                 Your no1 study weapon
             </h1>
+            <Button />
         </div>
     );
 };
---
+++ src/Button.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+    return <button className="btn btn-primary mt-8">Get started</button>
+}
+
+export default Button;
