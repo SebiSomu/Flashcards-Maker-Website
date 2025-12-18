02eb28ceb73481a48c48889617906ae4c6efa873
SebiSomu <sebigsomu@gmail.com>
2025-12-18 18:26:59 +0200

    Succesfully set up TailwindCSS configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+    return (
+        <div className="min-h-screen bg-[#e0e7ff] flex flex-col items-center justify-center">
+            <h1 className="relative text-5xl md:text-7xl font-black text-slate-800 tracking-tighter inline-block
+                     after:content-[''] after:absolute after:left-0 after:bottom-1 after:w-full after:h-[4px] after:bg-slate-800">
+                Your no1 study weapon
+            </h1>
+        </div>
+    );
+};
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
